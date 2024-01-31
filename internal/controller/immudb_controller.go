8ec9ded934c2e6bc79e17132fa8208ddf3911ec1
Mathieu Cesbron <mathieu.cesbron@protonmail.com>
2024-01-31 21:30:26 +0100

    Add logger

--- internal/controller/immudb_controller.go
+++ internal/controller/immudb_controller.go
@@ -25,31 +25,21 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	immudbiov1 "github.com/MathieuCesbron/immudb-operator/api/v1"
+	"github.com/go-logr/logr"
 )
 
 // ImmudbReconciler reconciles a Immudb object
 type ImmudbReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Log    logr.Logger
 }
 
-//+kubebuilder:rbac:groups=immudb.io,resources=immudbs,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=immudb.io,resources=immudbs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=immudb.io,resources=immudbs/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Immudb object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
+// +kubebuilder:rbac:groups=immudb.io,resources=immudbs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=immudb.io,resources=immudbs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=immudb.io,resources=immudbs/finalizers,verbs=update
 func (r *ImmudbReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
+	r.Log = log.FromContext(ctx).WithName("Reconciler")
 
 	return ctrl.Result{}, nil
 }
